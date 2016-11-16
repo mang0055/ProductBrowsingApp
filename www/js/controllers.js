6e30f09416f718f652ee49dc4d7cafd4078b2b1b
Raviraj Mangukiya <ravirajcm@gmail.com>
2016-11-15 21:31:14 -0500

    Added Rating, Localstorage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,17 @@
 angular.module('starter.controllers', [])
+    .controller('CategoriesCtrl', function ($scope, $stateParams, fetchCategory) {
+        fetchCategory.fetch().then(function (data) {
+            $scope.data = data;
+        });
 
-.controller('CategoriesCtrl', function($scope,$stateParams,fetchCategory) {
-      fetchCategory.fetch().then(function(data) {
-       $scope.data = data;
-    });
-    
-})
-.controller('BookListCtrl', function($scope, $stateParams,fetchCategory) {
-    console.log("BookListCtrl");    
-    $scope.catID = $stateParams.catID;
+    })
+    .controller('BookListCtrl', function ($scope, $stateParams, fetchCategory) {
+        console.log("BookListCtrl");
+        $scope.catID = $stateParams.catID;
         console.log($scope.catID);
         $scope.bookdata = [];
         fetchCategory.fetch().then(function (data) {
-            $scope.data=data;
+            $scope.data = data;
             for (var i = 0; i < data.books.length; i++) {
                 if (data.books[i].cat_id == $scope.catID) {
                     $scope.bookdata.push(data.books[i]);
@@ -20,34 +19,49 @@ angular.module('starter.controllers', [])
             }
 
         });
-        $scope.toggleCategory=function(){
-            console.log("aa");
-        }
-    
-})
-.controller('BookDetailCtrl', function($scope, $stateParams, fetchCategory) {
+
+    })
+    .controller('BookDetailCtrl', function ($scope, $stateParams, fetchCategory, fetchFromLocal) {
         $scope.catID = $stateParams.catID;
+        $scope.bookID = $stateParams.bookID;
         $scope.bookdata = [];
         fetchCategory.fetch().then(function (data) {
             for (var i = 0; i < data.books.length; i++) {
                 if (data.books[i]._id == $stateParams.bookID) {
-                    $scope.currentbook=data.books[i];
+                    $scope.currentbook = data.books[i];
                 }
+            }
+            if (fetchFromLocal.get($scope.bookID) == null) {
+                $scope.ratingsObject.rating = $scope.currentbook.rating;
             }
 
         });
-    $scope.isEmpty = function(obj) {
-  for(var prop in obj) {
-      if(obj.hasOwnProperty(prop))
-          return false;
-  }
-  return true;
-};
-})
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+        $scope.isEmpty = function (obj) {
+            for (var prop in obj) {
+                if (obj.hasOwnProperty(prop))
+                    return false;
+            }
+            return true;
+        };
+        $scope.temprating = fetchFromLocal.get($scope.bookID);
+        $scope.ratingsObject = {
+            iconOn: 'ion-ios-star',
+            iconOff: 'ion-ios-star-outline',
+            iconOnColor: 'rgb(200, 200, 100)',
+            iconOffColor: 'rgb(200, 100, 100)',
+            rating: $scope.temprating,
+            minRating: 1,
+            callback: function (rating) {
+                $scope.ratingsCallback(rating);
+            }
+        };
 
-.controller('ProfileCtrl', function($scope) {
-  
+        $scope.ratingsCallback = function (rating) {
+            console.log('Selected rating is : ', rating);
+            fetchFromLocal.add(rating, $scope.bookID);
+        };
+    })
+
+.controller('ProfileCtrl', function ($scope) {
+
 });
--- www/js/services.js
+++ www/js/services.js
@@ -12,4 +12,40 @@ angular.module('starter.services', [])
     };
     ;
     return fetchCategory;
-  });
+  })
+.service('fetchFromLocal',function($localStorage){
+    //Took reference from https://medium.com/@petehouston/awesome-local-storage-for-ionic-with-ngstorage-c11c0284d658#.s87l8eqti
+  $localStorage = $localStorage.$default({
+		rating: [],
+		product: []
+	});
+
+	var _add = function (rate, catId) {
+		for (var i = 0; i < $localStorage.product.length; i++) {
+			if (catId == $localStorage.product[i]) {
+				$localStorage.rating.splice(i, 1)
+				$localStorage.product.splice(i, 1)
+
+			}
+		}
+		$localStorage.rating.push(rate);
+		$localStorage.product.push(catId)
+
+	}
+	var _get = function (catId) {
+		if ($localStorage.product.length > 0) {
+			for (var i = 0; i < $localStorage.product.length; i++) {
+				if (catId == $localStorage.product[i]) {
+					return $localStorage.rating[i]
+				}
+			}
+		} else {
+			return null
+		}
+
+	}
+	return {
+		add: _add,
+		get: _get
+	};  
+});
